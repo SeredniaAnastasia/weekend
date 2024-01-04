e962f644767d2907a1e2f52a772a6a049587567b
Serednii Mykola <seredniimykola@gmail.com>
2024-01-04 23:57:13 +0100

    add services

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,36 @@
-import ButtonComponent from '@/components/ButtonComponent'
+import $ from 'jquery'
+// import Swiper from 'swiper'
 
-const button = new ButtonComponent('.button')
+import 'slick-slider'
 
-button.element.addEventListener('click', () => {
-  window.scrollTo({
-    top: document.querySelector('.header').clientHeight,
-    behavior: 'smooth',
-  })
+// const rater = require('rater-js')
+
+// slider services
+$('.slider-services').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  // spaceBetween: 15,
+  dots: true,
+  arrows: false,
+  centerPadding: '10px',
+  variableWidth: true,
+  // centerMode: true,
+  // fade: true,
+  responsive: [
+    {
+      breakpoint: 1130,
+      settings: {
+        slidesToShow: 2,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
 })
